809864b7025df69eb9e3afc89ffd6acc2026b074
HONGKYUMIN (ANTHONY) <hisp1301@gmail.com>
2022-08-18 13:28:01 +0900

    추가) lv.1 짝수와 홀수

---
+++ src/Programmers/Programmers_Lv1/even_and_odd.java
@@ -0,0 +1,23 @@
+package Programmers.Programmers_Lv1;
+
+public class even_and_odd {
+
+	public static void main(String[] args) {
+		
+		System.out.println(solution(3));
+
+	}
+	
+	public static String solution(int num) {
+	        
+	        String answer = "";
+	        
+	        if(num % 2 == 0) {
+	        	answer = "Even";
+	        } else {
+	        	answer = "Odd";
+	        }
+	        
+	        return answer;
+	    }
+}
