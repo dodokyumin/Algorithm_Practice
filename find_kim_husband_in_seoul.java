6cf337d8c8fec4ace8a5165ec9c13409c6d2eb14
HONGKYUMIN (ANTHONY) <95400620+dodokyumin@users.noreply.github.com>
2022-08-19 18:57:34 +0900

    추가) lv.1 서울에서 김서방 찾기

---
+++ find_kim_husband_in_seoul.java
@@ -0,0 +1,23 @@
+package Programmers_Lv1;
+
+public class find_kim_husband_in_seoul {
+
+	public static void main(String[] args) {
+		find_kim_husband_in_seoul fkhis = new find_kim_husband_in_seoul();
+		String[] names = {"디두", "kim", "귬귬"};
+		System.out.println(fkhis.solution(names));
+		
+	}
+	 public String solution(String[] seoul) {
+	        String where = "";
+	        for(int i = 0; i < seoul.length; i++) {
+	        	if(seoul[i] == "Kim") {
+	        		where = String.valueOf(i);
+	        		break;
+	        	}
+	        }
+	        
+	        String answer = "김서방은 " + where + "에 있다";
+	        return answer;
+	    }
+}
